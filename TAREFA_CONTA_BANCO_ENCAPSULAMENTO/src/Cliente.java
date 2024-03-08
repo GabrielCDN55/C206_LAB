1fd5e629b379aa6a6c95bc5a0235b43c56971495
GabrielCDN55 <150607057+GabrielCDN55@users.noreply.github.com>
2024-03-08 02:33:59 -0300

    Aula 03 - Conta de banco - Encapsulamento

---
+++ TAREFA_CONTA_BANCO_ENCAPSULAMENTO/src/Cliente.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private Conta conta;
+    public boolean sacarDinheiro(double valor)
+    {
+        if(conta.getSaldo() >= valor)
+        {
+            conta.setSaldo(conta.getSaldo() - valor);
+            System.out.println("Saque bem sucedido!");
+            return true;
+        }
+        else
+        {
+            System.out.println("Saldo insufiente! Não foi possível sacar a quantia");
+            return false;
+        }
+    }
+    public void depositarDinheiro(double valor)
+    {
+        conta.setSaldo(conta.getSaldo() + valor);
+        System.out.println("Valor de " + valor + " adicionado a conta com sucesso!");
+    }
+    public boolean comprarComCredito(double valor)
+    {
+        if(conta.getLimite() >= valor)
+        {
+            System.out.println("Compra realizada com sucesso!");
+            return true;
+        }
+        else
+        {
+            System.out.println("Compra não realizada. Verifique o limite!");
+            return false;
+        }
+    }
+    public boolean transferir(Conta contaParaReceber)
+    {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o valor que deseja transferir:");
+        double valorTranferido = entrada.nextDouble();
+
+        if(this.conta.getSaldo() >= valorTranferido)
+        {
+            this.conta.setSaldo(conta.getSaldo() - valorTranferido);
+            contaParaReceber.setSaldo(conta.getSaldo() + valorTranferido);
+            System.out.println("Transferencia realizada!");
+            return true;
+        }
+        else
+        {
+            System.out.println("Transferencia não realizada! Saldo insuficiente");
+            return false;
+        }
+    }
+    public String toString()
+    {
+        return "Nome:" + nome + " CPF:" + cpf + " Dados bancários - " + conta.toString();
+    }
+
+    public Cliente(String nome, String cpf,double limite, double saldo) {
+        conta = new Conta(limite, saldo);
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+}
---
+++ TAREFA_CONTA_BANCO_ENCAPSULAMENTO/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Gabriel", "5555555", 1000, 4000);
+        Cliente cliente2 = new Cliente("Paulo","44444", 300, 600);
+
+        System.out.println("Cliente1: " + cliente1.toString());
+        System.out.println("Cliente2: " + cliente2.toString());
+
+        cliente1.depositarDinheiro(1000);
+        cliente1.sacarDinheiro(4000);
+        cliente1.comprarComCredito(5000);
+
+        System.out.println("Saldo conta: " + cliente1.toString());
+    }
+}
