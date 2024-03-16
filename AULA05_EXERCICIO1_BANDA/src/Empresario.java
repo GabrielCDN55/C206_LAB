64bd756df5f53d2b96b50d4e509c439dc89a0ad1
GabrielCDN55 <150607057+GabrielCDN55@users.noreply.github.com>
2024-03-15 23:35:00 -0300

    Delete AULA05_EXERCICIO1_BANDA directory

--- AULA05_EXERCICIO1_BANDA/src/Empresario.java
+++
@@ -1,9 +0,0 @@
-public class Empresario {
-    private String nome;
-    private long cnpj;
-
-    public Empresario(String nome, long cnpj) {
-        this.nome = nome;
-        this.cnpj = cnpj;
-    }
-}
